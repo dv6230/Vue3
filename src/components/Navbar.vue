62c5ab36ad8dc566ce4ec39b2cbaa3239478ef74
allisonhung <allisonhung@iscom.com.tw>
2022-09-21 12:00:38 +0800

    Login 頁面製作

---
+++ src/components/Navbar.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <nav class="navbar navbar-expand-lg bg-light">
+      <div class="container-fluid">
+        <router-link class="navbar-brand" to="/">Navbar</router-link>
+        <!--        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"-->
+        <!--                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">-->
+        <!--          <span class="navbar-toggler-icon"></span>-->
+        <!--        </button>-->
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <router-link class="nav-link" to="/login">Home</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/login">Link</router-link>
+            </li>
+          </ul>
+          <form class="d-flex" role="search">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <router-link class="nav-link" to="/login">登入</router-link>
+              </li>
+            </ul>
+          </form>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "navbarHeader",
+  created() {
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="d-flex justify-content-center align-items-center" style="height: 80vh">
+    <form class="col-md-3">
+      <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">帳號</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" required>
+        <!--        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>-->
+      </div>
+      <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">密碼</label>
+        <input type="password" class="form-control" id="exampleInputPassword1" required>
+      </div>
+      <!--      <div class="mb-3 form-check">-->
+      <!--        <input type="checkbox" class="form-check-input" id="exampleCheck1">-->
+      <!--        <label class="form-check-label" for="exampleCheck1">Check me out</label>-->
+      <!--      </div>-->
+      <div class="d-flex justify-content-center mt-2">
+        <button type="submit" class="btn btn-primary col-6 ">送出</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+
+// use async function
+// takes promise and returns a promise
+// import { asyncLoading } from 'vuejs-loading-plugin'
+
+export default {
+  name: "LoginView",
+  created() {
+    // set loading state manually in components
+    this.$loading(true)
+    this.$loading(false)
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
