4c186de227db25b1694e52ae80c1663aef5774fa
allisonhung <allisonhung@iscom.com.tw>
2022-09-21 12:00:15 +0800

    加入 loading 套件

--- src/main.js
+++ src/main.js
@@ -2,8 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 import router from './router'
+import "bootstrap"; // 從nodeModule中載入Bootstrap
+import "bootstrap/dist/css/bootstrap.min.css"; // Import css file
+import "jquery"
+import VueLoading from 'vuejs-loading-plugin'
 
 Vue.config.productionTip = false
+
+// using default options
+Vue.use(VueLoading)
 
 new Vue({
   store,
